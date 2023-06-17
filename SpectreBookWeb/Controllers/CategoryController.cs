2ff2731609ce4dbf5583d6a14f807868792d8317
Jawad Mansour <mansour.j930@gmail.com>
2023-06-18 00:11:28 +0300

    Add project files.

---
+++ SpectreBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SpectreBookWeb.Data;
+using SpectreBookWeb.Models;
+namespace SpectreBookWeb.Controllers
+{
+   
+    public class CategoryController : Controller
+    {
+        private readonly AppDBContext _db;
+
+        public CategoryController(AppDBContext db)
+        {
+            _db = db;  
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList= _db.Categories;
+            return View(objCategoryList);
+        }
+
+        //Get
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+
+        //Post
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SpectreBookWeb/Data/AppDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SpectreBookWeb.Models;
+
+namespace SpectreBookWeb.Data
+{
+    public class AppDBContext :DbContext
+    {
+        public AppDBContext(DbContextOptions<AppDBContext> options) : base(options)
+        {  
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
